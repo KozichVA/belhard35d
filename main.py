e385e1005a4575c231b540bddea153d1c4fa5f77
KozichVA <Vadim.kozich@mail.ru>
2022-11-01 19:40:03 +0300

    burime open file

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ while i != index:
         littera.append(Letter)
 
 print(littera)
+russion_world = []
+filename = "russian.txt"
+#Тут вообще чушь, но, слава богу, файл открылся.
+for world in open(filename, 'r', encoding='utf-8'):
+    russion_world.append(world)
+#Тут есть ещё проблема с utf-8
+
+print("ok, что-то ещё напишу... и!")
