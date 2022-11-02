fb7ca07c0c50994a538bb4259fdb61776e8d577d
KozichVA <Vadim.kozich@mail.ru>
2022-11-03 02:06:16 +0300

    burime open file

---
+++ les3.py
@@ -0,0 +1,8 @@
+# text = input("вводи текст эксперементируй ")
+text = "Мой дядя самых честных правил, когда не в шутку занемог..."
+print("функция \"len\" - сколько символов len(text) = ", len(text))
+input(">")
+print("функция \"split\", преобразовывет текст в список text split(\" \") = \n", text.split(" "))
+input(">")
+word = text.split(" ")
+print("функция \"join\", список text split(\" \") = \n", text.split(" "))
--- main.py
+++ main.py
@@ -7,13 +7,13 @@ while i != index:
     for Letter in text[i]:
         i += 1
         littera.append(Letter)
+print(f"Ваше слово состоит из {index} букв: {littera}")
 
-print(littera)
 russion_world = []
 filename = "russian.txt"
-# Тут вообще чушь, но, слава богу, файл открылся.
-for world in open(filename, 'r', encoding='utf-8'):
+for world in open(filename, 'r'):
     russion_world.append(world)
-# Тут есть ещё проблема с utf-8
 
-print("ok, что-то ещё напишу... и! Да блин, что я неправильно делаю")
+# print(russion_world[322])
+# print("".join(littera))
+# print(littera.join(" "))   !!!прикольная ошибка!!!
