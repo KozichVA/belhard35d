5059d4a2955eb64b82f0ed4a335018602430c246
KozichVA <Vadim.kozich@mail.ru>
2022-11-03 02:50:38 +0300

    work with text

--- les3.py
+++ les3.py
@@ -2,7 +2,14 @@
 text = "Мой дядя самых честных правил, когда не в шутку занемог..."
 print("функция \"len\" - сколько символов len(text) = ", len(text))
 input(">")
-print("функция \"split\", преобразовывет текст в список text split(\" \") = \n", text.split(" "))
+print("функция \"split\", преобразует текст в список text.split(\" \") = \n", text.split(" "))
 input(">")
 word = text.split(" ")
-print("функция \"join\", список text split(\" \") = \n", text.split(" "))
+print("функция \"join\", список word (\" \").join(word) = ",(" ").join(word))
+input(">")
+print("функция \"join\", текст text (\"-\").join(text) = ", ("-").join(text))
+search = input("ВНИМАНИЕ! Введи слово или символ, который будешь искать: ")
+print('ищем совпадени в тексте text.find(search,[start],[end]. результат = ', text.find(search))
+print('ищем совпадени в тексте text.rfind(search,[start],[end]. результат = ', text.rfind(search))
+
+
