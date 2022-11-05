02ff9356d8d08d4cf570ab93c04bab02f5e1cce5
KozichVA <Vadim.kozich@mail.ru>
2022-11-05 19:07:29 +0300

    les4 ex1 - done ex2 - done ex3 - ???

--- les_4.py
+++ les_4.py
@@ -1,5 +1,5 @@
 # Заполнить список степенями числа 2 (от 2^1 до 2^n).
-n = input("введите n: ")
+n = input("введите n (используется в 1-ом и 3-ем задании): ")
 degree_two = []
 for i in range(int(n)):
     degree_two.append(2 ** i)
@@ -16,3 +16,16 @@ for i in range(len(text_control)):
     letter_control[text_control[i]] = num
 
 print(letter_control)
+
+# Заполнить словарь где ключами будут выступать числа от 0 до n, а
+# значениями вложенный словарь с ключами "name" и "email", а значения
+# для этих ключей будут браться с клавиатуры
+print('\nПытка с заполнением адресной книги начинается...')
+book = {'имя': 'почта'}
+strange_book = {'номер': book}
+for i in range(int(n)):
+    name = input(f"Впишите имя {i}: ")
+    email = input(f"Впишите e-mail {i}: ")
+    book[name] = email
+    strange_book[i] = book
+print(strange_book)
