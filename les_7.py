99cae41c66a632b2971835d253011fab8263c0e3
KozichVA <Vadim.kozich@mail.ru>
2022-11-17 23:23:31 +0300

    ex 1 - - ex 2 - done ex 3 - done ex 4 - done

--- les_7.py
+++ les_7.py
@@ -78,11 +78,49 @@ print(f'''{i25} - монет по 25 копеек
 
 # 3. Написать функцию pow, которая принимает число А и число Б, необходимо с помощью
 # рекурсии возвести число А в степень Б
+def mulityplay(a,b,c = 1):
+    c *= a
+    b -= 1
+    if b:
+        return mulityplay(a, b, c)
+    else:
+        return c
 
-
+print(mulityplay(int(input('введи А: ')), int(input('введи Б: '))))
 
 # 4. Петя перешёл в другую школу. На уроке физкультуры ему понадобилось определить своё
 # место в строю. Помогите ему это сделать. Программа получает на вход невозрастающую
 # последовательность натуральных чисел, означающих рост каждого человека в строю. После
 # этого вводится число X – рост Пети. Все числа во входных данных натуральные и не
 # превышают 200.
+# spisok = [192, 160, 149, 171, 120, 200, 180, 167, 153]
+# petia = int((input('рост Пети: ')))
+# spisok.append(petia)
+# spisok.sort(reverse = True)
+# print(spisok)
+#
+# for i in range(len(spisok)):
+#     print(i)
+#     if spisok[i] == petia and i == 0:
+#         print('Петя, ты самый высокий')
+#     elif spisok[i] == petia and i == len(spisok) - 1:
+#         print('Иди в конец, Петя!')
+#     elif spisok[i] == petia and petia != 200 and not i == 0:
+#         print(f'Петя, становись между {i}-ым и {i + 2}-ым')
+#     elif spisok.count(petia) > 1 and spisok[i] == petia:
+#         print(f'Петя, становись перед {i}-ым, вы одного роста')
+
+spisok = [192, 160, 149, 171, 120, 200, 180, 167, 153, 185]
+petia = int((input('рост Пети: ')))
+spisok.sort(reverse = True)
+print(spisok)
+
+for i in range(len(spisok)):
+    if spisok[i] == petia and i == 0:
+        print('Петя, ты самый высокий')
+    elif petia < spisok[i] and i == len(spisok) - 1:
+        print('Иди в конец, Петя!')
+    elif spisok[i] > petia and spisok[i+1] < petia:
+        print(f'Петя, становись между {i+1}-ым и {i + 2}-ым')
+    elif spisok[i] == petia:
+        print(f'Петя, становись перед {i+1}-ым, вы одного роста')
