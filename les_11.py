41e09e733bc939e666b8b50e10b467f4927b1f6a
KozichVA <Vadim.kozich@mail.ru>
2022-12-06 17:54:41 +0300

    Заработало

--- les_11.py
+++ les_11.py
@@ -6,50 +6,55 @@ cur = conn.cursor()
 cur.execute('''
 CREATE TABLE IF NOT EXISTS statuses(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-name VARCHAR(10) UNIQUE)''')
+name VARCHAR(10) UNIQUE);''')
 conn.commit()
 
 cur.execute('''
-CREATE TABLE IF NOT EXISTS orders(
-id INTEGER PRIMARY KEY,
-FOREIGN KEY (id) REFERENCES order_items(order_id),
-user_id INTEGER AUTOINCREMENT,
-status_id INTEGER,
-FOREIGN KEY (status_id) REFERENCES statuses(id))''')
-conn.commit()
+CREATE TABLE IF NOT EXISTS categories(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name VARCHAR(36) UNIQUE);''')
 
 cur.execute('''
 CREATE TABLE IF NOT EXISTS users(
 id INTEGER PRIMARY KEY,
-FOREIGN KEY (id) REFERENCES orders(user_id)
 name VARCHAR(24),
-email VARCHAR(24) UNIQUE)''')
+email VARCHAR(24) UNIQUE);''')
+conn.commit()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS orders(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+user_id INTEGER,
+status_id INTEGER, 
+FOREIGN KEY (user_id) REFERENCES user(id),
+FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE CASCADE
+);
+''')
+conn.commit()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS products(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+title VARCHAR(36),
+descr VARCHAR(140),
+category_id INTEGER,
+FOREIGN KEY (category_id) REFERENCES categories(id));
+''')
 conn.commit()
 
 cur.execute('''
 CREATE TABLE IF NOT EXISTS order_items(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 order_id INTEGER,
-FOREIGN KEY (order_id) REFERENCES orders(id)
 product_id INTEGER,
-FOREIGN KEY (product_id) REFERENCES products(id))''')
+FOREIGN KEY (order_id) REFERENCES orders(id),
+FOREIGN KEY (product_id) REFERENCES products(id));''')
 conn.commit()
 
 
-cur.execute('''
-CREATE TABLE IF NOT EXISTS products(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-titile VARCHAR(36)
-descr VARCHAR(140)
-category_id INTEGER
-FOREIGN KEY (category_id) REFERENCES categories(id))
-''')
-conn.commit()
 
-cur.execute('''
-CREATE TABLE IF NOT EXISTS categories(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name VARCHAR(36) UNIQUE)''')
+
+
 
 
 # cur.execute(''' CREATE TABLE IF NOT EXISTS categories(
