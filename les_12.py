1299aa3d1236f1538f03836222c8e830cec47911
Vadim <vadim.kozich@mail.ru>
2022-12-08 16:12:55 +0300

    не работает

--- les_12.py
+++ les_12.py
@@ -10,9 +10,10 @@ class Category(Base):
 
 class Product(Base):
     __table__ = 'products'
+
     id = Column(INT, primary_key=True)
     name = Column(VARCHAR(64), nullable=False)
-    prise = Column(DECIMAL(8, 2), default=0.0)
+    prise = Column(DECIMAL(8, 2), default=1)
     is_published = Column(BOOLEAN, default=False)
     category_id = Column(INT, ForeignKey('categories.id', ondelete='CASCADE'), nullable=False)
 
