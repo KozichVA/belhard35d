f60b8406e5513072e46177a6f7465c19854196c4
Vadim <vadim.kozich@mail.ru>
2022-12-15 21:20:58 +0300

    mixin str 19

--- les_12.py
+++ les_12.py
@@ -5,7 +5,7 @@ Base = declarative_base()
 
 
 @declarative_mixin
-class Basemixin(object):
+class BaseMixin(object):
     engine = create_engine('postgresql://axlamon:axlamon@localhost:5432/newhomewok')
     _Session = sessionmaker(bind=engine)
 
@@ -14,7 +14,7 @@ class Basemixin(object):
     @staticmethod
     def create_sesion(func):
         def wraper(*args, **kwargs):
-            with Basemixin._Session() as session:
+            with BaseMixin._Session() as session:
                 return func(*args, **kwargs, session=session)
         return wraper
 
@@ -42,13 +42,15 @@ class Basemixin(object):
     def delete(self, session: Session = None) -> None:
         session.delete(self)
         session.commit()
-class Category(Basemixin, Base):
+
+
+class Category(BaseMixin, Base):
     __tablename__ = 'categories'
     # id = Column(INT, primary_key=True)
     name = Column(VARCHAR(64), nullable=False, unique=True)
 
 
-class Product(Basemixin, Base):
+class Product(BaseMixin, Base):
     __tablename__ = 'products'
 
     # id = Column(INT, primary_key=True)
@@ -58,27 +60,27 @@ class Product(Basemixin, Base):
     category_id = Column(INT, ForeignKey('categories.id', ondelete='CASCADE'), nullable=False)
 
 
-class Status(Basemixin, Base):
+class Status(BaseMixin, Base):
     __tablename__ = 'statuses'
     # id = Column(INT, primary_key=True)
     name = Column(VARCHAR(10), unique=True)
 
 
-class User(Basemixin, Base):
+class User(BaseMixin, Base):
     __tablename__ = 'users'
     # id = Column(INT, primary_key=True)
     name = Column(VARCHAR(24))
     email = Column(VARCHAR(24), unique=True)
 
 
-class Order(Basemixin, Base):
+class Order(BaseMixin, Base):
     __tablename__ = 'orders'
     # id = Column(INT, primary_key=True)
     user_id = Column(INT, ForeignKey('users.id', ondelete='CASCADE'))
     status_id = Column(INT, ForeignKey('statuses.id', ondelete='CASCADE'))
 
 
-class OrderItem(Basemixin, Base):
+class OrderItem(BaseMixin, Base):
     __tablename__ = 'order_items'
     # id = Column(INT, primary_key=True)
     order_id = Column(INT, ForeignKey('orders.id', ondelete='CASCADE'))
@@ -88,7 +90,6 @@ class OrderItem(Basemixin, Base):
         return self.name
 
 
-
-# Category.name = 'Холодильник'
+Category.name = 'Холодильник'
 Category.save()
 print(Category.get(1))
